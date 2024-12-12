3b2af40bb5322e75a2c58212ba17d8fe78e55f6e
Kyle Lomer <kylelomer@gmail.com>
2024-12-12 12:47:07 +0100

    Refactor test to use ephemeral port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,15 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -17,7 +19,12 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind to address");
-    let _ = tokio::spawn(server);
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind to address");
+    // spawn return value is not used, explicitly drop the returned future
+    std::mem::drop(tokio::spawn(server));
+    // Return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
