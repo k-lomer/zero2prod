38e9a7f2349dcd94441094cf9e2a0aa9ec35090c
Kyle Lomer <kylelomer@gmail.com>
2024-12-19 14:39:16 +0100

    Tracing final form

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,16 @@
 //! src/startup.rs
 
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection_pool.clone())
