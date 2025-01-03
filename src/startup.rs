16998302887151bc288fe9d8f199140cb51ee414
Kyle Lomer <kylelomer@gmail.com>
2025-01-03 14:47:29 +0100

    Skeleton subscriptions/confirm route

--- src/startup.rs
+++ src/startup.rs
@@ -2,7 +2,7 @@
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -61,6 +61,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(connection_pool.clone())
             .app_data(email_client.clone())
     })
