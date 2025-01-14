6f6cd917820675359f1fd8a83c607b92b86d9298
Kyle Lomer <kylelomer@gmail.com>
2025-01-14 12:07:41 +0100

    add login end point

--- src/startup.rs
+++ src/startup.rs
@@ -2,7 +2,7 @@
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
+use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -70,6 +70,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
+            .route("/login", web::get().to(login_form))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
